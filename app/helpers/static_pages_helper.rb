83b426e792e2c91a170eb209c6585b643a29c525
ryugomikami <mikamiryuugo@yahoo.co.jp>
2019-03-09 01:46:14 +0000

    ヘルパー作って、出勤ボタン押したら時間表示されて退勤の表示。ただヘルパーは修正する必要ありそう。退勤ボタンも作る必要あり

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,11 +3,28 @@ module StaticPagesHelper
     day.strftime('%m/%d') == Date.today.strftime('%m/%d')
   end
 
-  def attended(day)
+  def attended?(day)
     # day.strftime('%a, %d %b %Y') == current_user.attendances.where(recorded_on: Date.today.strftime('%a, %d %b %Y')).recorded_on.strftime('%a, %d %b %Y')
     # whereは複数取り出す。find_byは一つ
-    day.strftime('%a, %d %b %Y') == current_user.attendances.find_by(recorded_on: Date.today.strftime('%a, %d %b %Y')).recorded_on.strftime('%a, %d %b %Y')
-
+    return if current_user.attendances.find_by(recorded_on: Date.today.strftime('%a, %d %b %Y')).nil?
+    # 真偽判定する以前にこの式のコードでエラー出る。だから、ここでこそ、returnの出番じゃないのか！！！recorded_onがから（nil）の場合は、returnで終了
+    day.strftime('%a, %d %b %Y') == current_user.attendances.
+    find_by(recorded_on: Date.today.strftime('%a, %d %b %Y')).recorded_on.strftime('%a, %d %b %Y')
   end
+
+  #出勤の時、分を表示するヘルパー
+  def attend_time(day)
+    if attended?(day)
+      current_user.attendances.find_by(recorded_on: Date.today.strftime('%a, %d %b %Y')).attend_time.strftime("%H")
+    end
+  end
+
+  def attend_minute(day)
+    if attended?(day)
+      current_user.attendances.find_by(recorded_on: Date.today.strftime('%a, %d %b %Y')).attend_time.strftime("%M")
+    end
+  end
+
+
 
 end
